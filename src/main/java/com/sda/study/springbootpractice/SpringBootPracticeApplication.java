f5498e4989fbb1472ffea5a0643995dbe43dfdee
Kätlin <kpadesaar@gmail.com>
2023-02-21 20:58:49 +0200

    Added spring application properties

--- src/main/java/com/sda/study/springbootpractice/SpringBootPracticeApplication.java
+++ src/main/java/com/sda/study/springbootpractice/SpringBootPracticeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootPracticeApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(SpringBootPracticeApplication.class, args);
     }
 
 }
